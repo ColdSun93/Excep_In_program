51b316810c488a5512f191bf479a880f4d3c920d
aleksandr_sa <coldsun93@mail.ru>
2023-07-01 20:26:27 +0300

    add folder hw_3

---
+++ hw_3/People.java
@@ -0,0 +1,17 @@
+package hw_3;
+
+public class People {
+
+    private String name;
+    private String surname;
+    private String patronymic;
+    private int phone;
+    
+
+    protected People(String name, String surname, String patronymic, int phone) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.phone = phone;
+    }
+}
